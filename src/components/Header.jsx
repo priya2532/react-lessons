4934cac89109060deec54fb8179d2952442f53ee
priya2532 <23r11a66b3@gcet.edu.in>
2025-05-26 20:29:50 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,6 +10,7 @@ export default function Header() {
       -<Link to="/app7">App7</Link> -<Link to="/app8">App8</Link>-<Link to="/app9">App9</Link>
       -<Link to="/app10">App10</Link>-<Link to="/app11">App11</Link>-<Link to="/app12">App12</Link>-<Link to="/app13">App13</Link>-<Link to="/app14">App14</Link>
       -<Link to="/app15">App15</Link>
+      -<Link to="/app15">App15</Link>
     </div>
   );
 }
