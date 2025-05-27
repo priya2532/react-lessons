3b4fc2eb2fd05424f2ee38c3e680bb65e65226d2
priya2532 <23r11a66b3@gcet.edu.in>
2025-05-27 20:06:00 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import App16 from "./components/App16";
 import App17 from "./components/App17";
 import App18 from "./components/App18";
 import App19 from "./components/App19";
+import App20 from "./components/App20";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
@@ -48,6 +49,7 @@ function App() {
    <Route path="/app17" element={<App17 />} />
    <Route path="/app18" element={<App18 />} />
    <Route path="/app19" element={<App19 />} />
+   <Route path="/app20" element={<App20 />} />
 </Routes>
 
       <Footer />
